764dfa5c7f4bf2bcea2e372d649a09979c72ea58
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-11 22:53:38 +0300

    Add file with using unittest example.

---
+++ tests/test_abs_unittest.py
@@ -0,0 +1,16 @@
+"""This is my example of unittest using.
+The one test should fail, the other - pass."""
+
+import unittest
+
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+
+
+if __name__ == "__main__":
+    unittest.main()
