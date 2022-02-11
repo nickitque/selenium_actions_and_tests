52d85a3a289020453d9918c2d6347c3b7e144188
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-11 23:25:17 +0300

    Another test add.

---
+++ tests/test_with_pytest_raises_construction.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+"""Тест, позволяющий проверить, что на странице сайта 
+не должен отображаться какой-то элемент."""
+
+
+def test_exception1():
+    try:
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/")
+        with pytest.raises(NoSuchElementException):
+            browser.find_element(By.CSS_SELECTOR, "button.btn")
+            pytest.fail("Не должно быть кнопки Отправить")
+    finally:
+        browser.quit()
+
+
+def test_exception2():
+    try:
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/")
+        with pytest.raises(NoSuchElementException):
+            browser.find_element(By.CSS_SELECTOR, "no_such_button.btn")
+            pytest.fail("Не должно быть кнопки Отправить")
+    finally:
+        browser.quit()
