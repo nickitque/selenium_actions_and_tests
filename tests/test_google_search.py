1f6c331f5b89dfdeadb9536b33723783eceed5d3
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-17 01:36:19 +0300

    Add simple test for google search

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,8 +1,21 @@
-
-from selenium import webdriver
+from framework_for_stepic_tasks.methods_selenium import *
 
 
 def test_google_search():
+    """This is test presentation with using simple framework designed by me.
+    It's possible to make it more tiny and readable in future. This func is taking 4 strings."""
+    open_url("https://google.com")
+    SendKeys.by_class_name("gLFyf.gsfi", "QA Automation Engineer\n")
+    time_to_copy_result()
+    close_browser()
+
+
+def test_google_search_num_2():
+    """And this is test using standart functions. It takes more place.
+    This func is taking 6 strings."""
     driver = webdriver.Chrome()
     driver.get("https://google.com")
-    print("hhh")
+    search_input = driver.find_element(By.CLASS_NAME, "gLFyf.gsfi")
+    search_input.send_keys("QA Automation Engineer #2\n")
+    time.sleep(5)
+    driver.close()
